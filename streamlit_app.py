491425d68b041c87314860ce4c63f569bdf4c4f1
Mahantesh Hiremath <mahanteshimath@gmail.com>
2022-12-04 00:30:19 +0530

    pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('🥣Breakfast Menu')
 streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
@@ -6,5 +7,10 @@ streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+
+streamlit.dataframe(my_fruit_list)
+
 
    
