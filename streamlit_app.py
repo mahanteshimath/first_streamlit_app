4d22a580b9266a603199042f8cd9ee625c331d0f
Mahantesh Hiremath <mahanteshimath@gmail.com>
2022-12-04 00:32:46 +0530

    pick

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,11 @@ streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
+
 
 streamlit.dataframe(my_fruit_list)
 
